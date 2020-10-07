95f6c896a5a17a19a32d0d92b2867b74c94bb97f
iqbal125 <iqbal125@yahoo.com>
2020-10-06 19:09:16 -0700

    created env example

--- fargate_RDS/lib/aws_cdk_fargate-stack.ts
+++ fargate_RDS/lib/aws_cdk_fargate-stack.ts
@@ -2,6 +2,7 @@ import * as cdk from "@aws-cdk/core"
 import * as ec2 from "@aws-cdk/aws-ec2"
 import * as ecs from "@aws-cdk/aws-ecs"
 import * as rds from "@aws-cdk/aws-rds"
+import {Port} from "@aws-cdk/aws-ec2"
 
 require("dotenv").config()
 
@@ -15,20 +16,6 @@ export class AwsCdkFargateStack extends cdk.Stack {
     const cluster = new ecs.Cluster(this, "Cluster", {
       vpc,
     })
-
-    const db_name = process.env.DB_NAME
-    const db_password = process.env.DB_PASSWORD
-    const db_username = process.env.DB_USERNAME
-
-    const instance = new rds.DatabaseInstance(this, "Instance", {
-      engine: rds.DatabaseInstanceEngine.postgres({version: rds.PostgresEngineVersion.VER_10}),
-      vpc,
-      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.MICRO),
-      credentials: rds.Credentials.fromUsername(db_username, {password: db_password}),
-      databaseName: db_name,
-    })
-
-    //instance.connections.
 
     // Add capacity to it
     cluster.addCapacity("DefaultAutoScalingGroupCapacity", {
@@ -48,11 +35,25 @@ export class AwsCdkFargateStack extends cdk.Stack {
       logging,
     })
 
-    const securityGroup = new ec2.SecurityGroup(this, `security-group`, {
+    const securityGroupFargate = new ec2.SecurityGroup(this, `security-group`, {
       vpc,
     })
 
-    securityGroup.addIngressRule(ec2.Peer.ipv4("0.0.0.0/0"), ec2.Port.tcp(80))
+    securityGroupFargate.addIngressRule(ec2.Peer.ipv4("0.0.0.0/0"), ec2.Port.tcp(80))
+
+    const db_name = process.env.DB_NAME
+    const db_password = process.env.DB_PASSWORD
+    const db_username = process.env.DB_USERNAME
+
+    const instance = new rds.DatabaseInstance(this, "Instance", {
+      engine: rds.DatabaseInstanceEngine.postgres({version: rds.PostgresEngineVersion.VER_10}),
+      vpc,
+      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.MICRO),
+      credentials: rds.Credentials.fromUsername(db_username, {password: db_password}),
+      databaseName: db_name,
+    })
+
+    instance.connections.allowFrom(securityGroupFargate, ec2.Port.tcp(5432))
 
     // Instantiate an Amazon ECS Service
     const ecsService = new ecs.FargateService(this, "Service", {
