fad2c97bac7dadab39c8b0c6380aabc7cff174a3
iqbal125 <iqbal125@yahoo.com>
2020-11-19 16:57:54 -0800

    Update aws_cdk_ec2_rds-stack.ts

--- CICD with ALB ASG EC2/lib/aws_cdk_ec2_rds-stack.ts
+++ CICD with ALB ASG EC2/lib/aws_cdk_ec2_rds-stack.ts
@@ -1,67 +1,100 @@
 import * as cdk from "@aws-cdk/core"
 import * as ec2 from "@aws-cdk/aws-ec2"
-import * as elbv2 from "@aws-cdk/aws-elasticloadbalancingv2"
-import {AutoScalingGroup} from "@aws-cdk/aws-autoscaling"
 import * as codepipeline from "@aws-cdk/aws-codepipeline"
 import * as codepipeline_actions from "@aws-cdk/aws-codepipeline-actions"
 import * as codebuild from "@aws-cdk/aws-codebuild"
-import * as codedeploy from "@aws-cdk/aws-codedeploy"
-import {Bucket} from "@aws-cdk/aws-s3"
+import * as secretsmanager from "@aws-cdk/aws-secretsmanager"
+import * as iam from "@aws-cdk/aws-iam"
+import * as s3 from "@aws-cdk/aws-s3"
+import * as cloudfront from "@aws-cdk/aws-cloudfront"
+import * as origins from "@aws-cdk/aws-cloudfront-origins"
 
 require("dotenv").config()
 
-export class AwsCdkEc2RdsStack extends cdk.Stack {
+//1. Generate Github Access token
+//2. Set the Token AWS secrets manager console.
+//3. Get the arn
+
+export class InfraStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props)
-
-    //Start stack
 
     //create vpc
     const vpc = new ec2.Vpc(this, "VPC", {
       natGateways: 1,
+      maxAzs: 3,
     })
 
-    //create load balancer
-    const lb = new elbv2.ApplicationLoadBalancer(this, "LB", {
-      vpc,
-      internetFacing: true,
+    //Get Github access token
+    const secretGithub = secretsmanager.Secret.fromSecretAttributes(this, "ImportedSecret", {
+      secretArn: `arn:aws:secretsmanager:us-east-1:867137601660:secret:/github-access-token-NuotsR`,
+    })
+    const roleCodeBuild = new iam.Role(this, "CodeBuildRole", {
+      assumedBy: new iam.ServicePrincipal("codebuild.amazonaws.com"),
     })
 
-    //Create server access logs for bucket
-    const serverAccessLogsBucket = new Bucket(this, "MyFirstBucket")
-    lb.logAccessLogs(serverAccessLogsBucket)
-
-    //add load balancer listener
-    const listener = lb.addListener("Listener", {
-      port: 80,
-    })
-
-    //auto scaling group config
-    const asg = new AutoScalingGroup(this, "ASG", {
-      vpc,
-      instanceType: ec2.InstanceType.of(ec2.InstanceClass.BURSTABLE2, ec2.InstanceSize.MICRO),
-      machineImage: new ec2.AmazonLinuxImage(),
-    })
-
-    //create asg target group
-    const targets = listener.addTargets("ApplicationFleet", {
-      port: 8080,
-      targets: [asg],
-    })
+    secretGithub.grantRead(roleCodeBuild)
 
     // Source Stage with github
+    const owner = process.env.GITHUB_OWNER ? process.env.GITHUB_OWNER : "GithubOwner"
+    const repo = process.env.GITHUB_REPO ? process.env.GITHUB_REPO : "Repo"
+    const oauthToken = secretGithub.secretValueFromJson("GITHUB_ACCESS_TOKEN")
+
     const sourceOutput = new codepipeline.Artifact()
     const sourceAction = new codepipeline_actions.GitHubSourceAction({
       actionName: "GitHub_Source",
-      owner: process.env.GITHUB_OWNER,
-      repo: process.env.GITHUB_REPO,
-      oauthToken: process.env.GITHUB_ACCESS_TOKEN,
+      owner,
+      repo,
+      oauthToken,
+      branch: "main",
       output: sourceOutput,
     })
 
-    //Build Stage with nodejs app
+    //Build Stage
+    const buildSpecObj = {
+      version: "0.2",
+      phases: {
+        install: {
+          "runtime-versions": {
+            nodejs: 10,
+          },
+        },
+        pre_build: {
+          commands: ['echo "--------PREBUILD PHASE--------"', "npm install"],
+        },
+        build: {
+          commands: ["npm run build"],
+        },
+      },
+      artifacts: {
+        files: ["*/*"],
+        basedirectory: "build",
+      },
+    }
+
+    // const buildSpecObj2 = {
+    //  version: "0.2",
+    //  phases: {
+    //    install: {
+    //      "runtime-versions": {
+    //        nodejs: 10,
+    //      },
+    //    },
+    //    pre_build: {
+    //      commands: ['echo "--------PREBUILD PHASE--------"', "cd client", "npm install"],
+    //    },
+    //    build: {
+    //      commands: ["npm run build", "npm run export"],
+    //    },
+    //  },
+    //  artifacts: {
+    //    files: ["out/*"],
+    //    basedirectory: 'client'
+    //  },
+    //}
+
     const project = new codebuild.PipelineProject(this, "MyProject", {
-      buildSpec: codebuild.BuildSpec.fromObject({filename: "../buildspec.yml"}),
+      buildSpec: codebuild.BuildSpec.fromObject(buildSpecObj),
     })
 
     const buildOutput = new codepipeline.Artifact()
@@ -72,17 +105,19 @@ export class AwsCdkEc2RdsStack extends cdk.Stack {
       outputs: [buildOutput],
     })
 
-    //Deploy Stage to loadbalancer
-    const deploymentGroup = new codedeploy.ServerDeploymentGroup(this, "DeploymentGroup", {
-      loadBalancer: codedeploy.LoadBalancer.application(targets),
-    })
-
-    const deployAction = new codepipeline_actions.CodeDeployServerDeployAction({
-      actionName: "CodeDeploy",
+    //deploy stage
+    const outputBucket = new s3.Bucket(this, "BuildOutputBucket", {})
+    const deployAction = new codepipeline_actions.S3DeployAction({
+      actionName: "S3Deploy",
+      bucket: outputBucket,
       input: buildOutput,
-      deploymentGroup,
     })
 
+    new cloudfront.Distribution(this, "myDist", {
+      defaultBehavior: {origin: new origins.S3Origin(outputBucket)},
+    })
+
+    //complete pipeline
     new codepipeline.Pipeline(this, "MyPipeline", {
       stages: [
         {
